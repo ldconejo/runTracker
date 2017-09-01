760b9b998f633381b47f5ca104edd185a4ece1ab
Luis Conejo-Alpizar <luisconej@gmail.com>
2017-09-01 09:30:30 -0700

    Added support for login and database operations for new users using Firebase

--- index.ios.js
+++ index.ios.js
@@ -4,6 +4,7 @@
  * @flow
  */
 
+import * as firebase from 'firebase';
 import React, {Component} from 'react';
 import {
   AppRegistry,
@@ -20,6 +21,10 @@ import { StackNavigator } from 'react-navigation';
 
 // Home screen
 class HomeScreen extends React.Component {
+  constructor(props) {
+    super(props);
+    Firebase.initialise();
+  }
   static navigationOptions = {
     title: 'Welcome to 30Palos',
   };
@@ -61,20 +66,39 @@ class LoginScreen extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-                  userID: 'None',
-                  myPassword: 'None'
+                  userEmail: 'None',
+                  myPassword: 'None',
+                  response: 'Not logged in',
                 };
   }
   static navigationOptions = {
     title: 'Log into your account',
   };
+  async login() {
+
+      try {
+          await firebase.auth().signInWithEmailAndPassword(this.state.userEmail, this.state.myPassword);
+
+          this.setState({
+              response: "Logged In!"
+          });
+            const { navigate } = this.props.navigation;
+            navigate('RegularUser');
+      } catch (error) {
+          this.setState({
+              response: error.toString()
+          })
+      }
+
+  }
   render() {
+    const { navigate } = this.props.navigation;
     return (
       <View style={{padding: 10}}>
         <TextInput
           style={{height: 40}}
-          placeholder="Enter your user ID"
-          onChangeText={(userID) => this.setState({userID})}
+          placeholder="Enter your email"
+          onChangeText={(userEmail) => this.setState({userEmail})}
         />
         <TextInput secureTextEntry={true}
           style={{height: 40}}
@@ -83,7 +107,9 @@ class LoginScreen extends React.Component {
         />
         <Button
           title="Submit"
+          onPress={() => this.login()}
         />
+        <Text>{this.state.response}</Text>  
       </View>
     );
   }
@@ -95,18 +121,39 @@ class NewUserScreen extends React.Component {
     super(props);
     this.state = {
                   userID: 'None',
-                  name: 'John',
-                  lastName: 'Doe',
-                  manager: 'None',
-                  role: 'Regular User',
+                  userEmail: 'None',
+                  myPassword: 'None',
+                  userName: 'John',
+                  userLastName: 'Doe',
+                  userManager: 'None',
+                  userRole: 'Regular User',
+                  response: 'Hola',
                 };
   }
   static navigationOptions = {
     title: 'Create New User',
   };
+  async signup() {
+
+        try {
+            await firebase.auth().createUserWithEmailAndPassword(this.state.userEmail, this.state.myPassword);
+
+            this.setState({
+                response: "account created"
+            });
+            // Add remaining user data to the database
+            Database.setUserData(this.state.userID, this.state.userEmail, this.state.userName, this.state.userLastName, this.state.userManager, this.state.userRole);
+
+        } catch (error) {
+            this.setState({
+                response: error.toString()
+            })
+        }
+
+    }
   render() {
     return (
-      <View>
+      <View style={{padding: 10}}>
         <TextInput
           style={{height: 40}}
           placeholder="Enter user ID"
@@ -114,29 +161,41 @@ class NewUserScreen extends React.Component {
         />
         <TextInput
           style={{height: 40}}
+          placeholder="Enter email"
+          onChangeText={(userEmail) => this.setState({userEmail})}
+        />
+        <TextInput secureTextEntry={true}
+          style={{height: 40}}
+          placeholder="Enter your password"
+          onChangeText={(myPassword) => this.setState({myPassword})}
+        />
+        <TextInput
+          style={{height: 40}}
           placeholder="Enter name"
-          onChangeText={(name) => this.setState({name})}
+          onChangeText={(userName) => this.setState({userName})}
         />
         <TextInput
           style={{height: 40}}
           placeholder="Enter last name"
-          onChangeText={(lastName) => this.setState({lastName})}
+          onChangeText={(userLastName) => this.setState({userLastName})}
         />  
         <TextInput
           style={{height: 40}}
           placeholder="Manager (optional)"
-          onChangeText={(manager) => this.setState({manager})}
+          onChangeText={(userManager) => this.setState({userManager})}
         />  
         <Picker
-          selectedValue={this.state.role}
-          onValueChange={(itemValue, itemIndex) => this.setState({role: itemValue})}>
+          selectedValue={this.state.userRole}
+          onValueChange={(itemValue, itemIndex) => this.setState({userRole: itemValue})}>
           <Picker.Item label="Regular User" value="Regular" />
           <Picker.Item label="Manager" value="Manager" />
           <Picker.Item label="Admin" value="Admin" />
         </Picker>
         <Button
           title="Create"
-        />                      
+          onPress={() => this.signup()}
+        />  
+        <Text>{this.state.response}</Text>                    
       </View>
     );
   }
@@ -214,6 +273,39 @@ class MyActivityScreen extends React.Component {
         <Text>Placeholder</Text>
       </View>
     )
+  }
+}
+
+// Initialize Firebase
+// Code from: https://github.com/JamesMarino/Firebase-ReactNative/blob/master/includes/firebase/firebase.js
+class Firebase {
+
+  /**
+   * Initialises Firebase
+   */
+  static initialise() {
+      firebase.initializeApp({
+        apiKey: "AIzaSyAFFb0sQCmtga-78QrG5QUuFKBOk-YCYmo",
+        authDomain: "palos-5bd92.firebaseapp.com",
+        databaseURL: "https://palos-5bd92.firebaseio.com",
+        projectId: "palos-5bd92",
+        storageBucket: "palos-5bd92.appspot.com",
+        messagingSenderId: "204756878847"
+      });
+  }
+}
+
+// Database operations
+class Database {
+  static setUserData(userID, userEmail, userName, userLastName, userManager, userRole) {
+    let userPath = "/user/" + userID + "/details";
+    return firebase.database().ref(userPath).set({
+            userEmail: userEmail,
+            userName: userName,
+            userLastName: userLastName,
+            userManager: userManager,
+            userRole: userRole,
+        })
   }
 }
 
